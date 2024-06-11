e23e3c60e25fe62cbc0f89e6a74ed46f92e19767
Alexis Hall <135035822+AleyH30@users.noreply.github.com>
2024-06-10 18:33:33 -0700

    update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Intro from "./0Components/Intro/Intro";
 import Featured from "../0Components/Featured/Featured";
 import './Home.css';
 
 const Home = () => {
+  
+  useEffect(() => {
+    window.scrollTo(0,0)
+  }, [])
+
     return (
         <div className="home-page">
           <Intro/>
